f60c76b588415bea31cb4a276cc0247c45791be7
47Shiqi <147263794+47Shiqi@users.noreply.github.com>
2025-05-08 09:43:06 -0400

    Update python.py

--- python.py
+++ python.py
@@ -8,5 +8,20 @@ app = Flask(__name__)
 def main():
     return render_template("login.html")
 
+@app.route("/login",methods=["POST"])
+def GetInfo():
+    global username,userpasswd
+    global fileDir
+    fileDir = os.path.dirname(os.path.realpath("__file__"));
+
+    username = request.form.get('txtusername');
+    userpasswd = request.form.get('txtpassword');
+    usertype = request.form.get('usertype');
+
+    if(usertype == "admin"):
+        return render_template("AdminHome.html",username = username);
+    else:
+        return render_template("UserHome.html",username = username);
+
 if __name__ == "__main__":
     app.run()
